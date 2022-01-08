d93c943b60e56aaeea4a566aecfcaf3c664356e1
eugene-bakulin <bakulin.eugene@gmail.com>
2022-01-08 17:36:59 +0400

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+  let reverseNumber = '';
+  if (String(n)[0] != '-') {
+    for (let i = String(n).length - 1; i >= 0; i --) {
+        reverseNumber += String(n)[i];
+    }
+  } else {
+    for (let i = String(n).length - 1; i >= 1; i --) {
+        reverseNumber += String(n)[i];
+    }
+  }
+  return +reverseNumber;
 }
